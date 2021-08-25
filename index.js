b8b2566012877f5ca6816ebbada1f29c4ab4ca3d
Andrew <a.m.romanyuk@gmail.com>
2021-08-25 03:08:32 +0300

    Fix linter errors

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const logger = require('./src/logger');
 const app = express();
 const port = 8010;
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
